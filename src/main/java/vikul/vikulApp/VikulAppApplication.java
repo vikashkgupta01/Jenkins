98b0082708bc320c55383dbdbc02835f157a6900
vikashkgupta01 <vikashkgupta01@gmail.com>
2024-11-17 20:21:32 +0530

    First Commit as simple framework

---
+++ src/main/java/vikul/vikulApp/VikulAppApplication.java
@@ -0,0 +1,15 @@
+package vikul.vikulApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VikulAppApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Insdide Mian!!");
+		SpringApplication.run(VikulAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vikul/vikulApp/VikulAppApplicationTests.java
@@ -0,0 +1,13 @@
+package vikul.vikulApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VikulAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
